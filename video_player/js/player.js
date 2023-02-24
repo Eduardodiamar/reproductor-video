69fce25e98051c75d99244c2402aead091cc06ca
Eduardodiamar <101567070+Eduardodiamar@users.noreply.github.com>
2023-02-24 20:30:40 +0100

    wip

--- video_player/js/player.js
+++ video_player/js/player.js
@@ -9,18 +9,46 @@ function startplayer() {
 }
 
 function play_vid() {
+
     player.play();
+    isPlaying = true
+    showPlayPause()
+
 }
 
 function pause_vid() {
     player.pause();
+    isPlaying = false
+    showPlayPause()
 }
 
 function stop_vid() {
     player.pause();
     player.currentTime = 0;
+    isPlaying = false
 }
 
 function change_vol() {
     player.volume = document.getElementById("change_vol").value;
+}
+isPlaying = false
+function showPlayPause(){
+
+    if (isPlaying) {
+        document.getElementById('play_button').style.display = "none"
+        document.getElementById('pause_button').style.display = "flex"
+        console.log('playing');
+    } else {
+        document.getElementById('play_button').style.display = "flex"
+        document.getElementById('pause_button').style.display = "none"
+        console.log(' not playing');
+
+    }
+}
+function hiddeButtons(){
+    
+        document.getElementById('play_button').style.display = "none"
+        document.getElementById('pause_button').style.display = "none"
+    
+    
 }
